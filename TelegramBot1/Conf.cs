32c7de42a54885b1113af9f41582e34ac219aefe
betterloveu <d2ilstuff@gmail.com>
2021-05-21 17:47:01 +0300

    st-downTbot

---
+++ TelegramBot1/Conf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot1
+{
+    public static class Conf
+    {
+        public static string token = "1822527670:AAEMgQQF1TouCemZCt5qvbc9ynm_q3xvGAw";
+    }
+}
---
+++ TelegramBot1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBot1
+{
+    class Program
+    {
+        private static ITelegramBotClient tClient;
+        static void Main(string[] args)
+        {
+            tClient = new TelegramBotClient(Conf.token);
+
+            var me = tClient.GetMeAsync().Result;
+            tClient.OnMessage += Bot_OnMessage;
+            tClient.StartReceiving();
+
+            Console.ReadKey();
+
+            tClient.StopReceiving();
+        }
+
+
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            var mesT = e.Message;
+            if (mesT.Text != null)
+            {
+                Console.WriteLine($"Сообщение: {mesT.Text}" );
+                await tClient.SendTextMessageAsync(mesT.Chat.Id, mesT.Text, replyMarkup: GetButtons(), replyToMessageId: mesT.MessageId);
+                //switch (mesT.Text)
+                //{
+                //    case "Картинка":
+                //            var pic = await tClient.SendPhotoAsync(
+                //                chatId: mesT.Chat.Id, photo: "https://tgram.ru/wiki/stickers/img/Uwik_stikers/png/1.png", replyToMessageId: mesT.MessageId, replyMarkup: GetButtons());
+                //            break;
+                //    default: await tClient.SendTextMessageAsync(mesT.Chat.Id, "", replyMarkup: GetButtons());
+                //        break;
+                //}
+
+            }
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton>{ new KeyboardButton {Text = "Привет"}, new KeyboardButton {Text = "Пока"} },
+                    new List<KeyboardButton>{ new KeyboardButton { Text = "Как дела?" }, new KeyboardButton {Text = "Что делаешь?" } }
+                }
+            };
+        }
+    }
+    
+}
+
